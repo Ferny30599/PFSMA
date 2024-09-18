ca928088e19cf3a212647409c025b57cbbfd3ac9
Fernando Abel Navarro-Martínez <93708337+Ferny30599@users.noreply.github.com>
2024-09-18 00:50:38 -0600

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ferny/smapf/src
-# Build directory: /home/ferny/smapf/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("RMD")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ferny/smapf/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ferny/smapf/devel;/home/ferny/SMA/devel;/opt/ros/noetic'.split(';') if '/home/ferny/smapf/devel;/home/ferny/SMA/devel;/opt/ros/noetic' != '' else []
