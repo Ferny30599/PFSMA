a57cd1653bebadad90651b03a2e56a48a2b56251
Fernando Abel Navarro-Martínez <93708337+Ferny30599@users.noreply.github.com>
2024-09-18 00:53:32 -0600

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ferny/smacp/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ferny/smacp/devel;/home/ferny/smapf/devel;/home/ferny/SMA/devel;/opt/ros/noetic'.split(';') if '/home/ferny/smacp/devel;/home/ferny/smapf/devel;/home/ferny/SMA/devel;/opt/ros/noetic' != '' else []
