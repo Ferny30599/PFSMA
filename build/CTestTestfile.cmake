32385296094b5b6556e9a55cf99d6737531527e6
Fernando Abel Navarro-Martínez <93708337+Ferny30599@users.noreply.github.com>
2024-09-18 00:26:10 -0600

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ferny/smapf/src
+# Build directory: /home/ferny/smapf/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("RMD")
