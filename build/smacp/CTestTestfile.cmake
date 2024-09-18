4c09335918fff2dddb2636a0e6615ef5585b6c8e
Fernando Abel Navarro-Martínez <93708337+Ferny30599@users.noreply.github.com>
2024-09-18 00:54:44 -0600

    Add files via upload

---
+++ build/smacp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ferny/smacp/src/smacp
+# Build directory: /home/ferny/smacp/build/smacp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
